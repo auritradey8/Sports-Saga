89f24374ad2c64b0a3a49ec1155291cef2c91148
Ameya Srivastava <0ameya.sr@gmail.com>
2024-03-23 15:54:44 +0530

    completed server routing and cart mechanism

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template
+from flask import *
 app = Flask(__name__)
+
+user_credentials = [("user@gmail.com","password")]
+session_exists = False
 
 @app.route("/")
 def index():
@@ -16,3 +19,18 @@ def purchase():
 @app.route("/login")
 def login():
     return render_template("login.html")
+ 
+@app.route("/session",methods=["POST"])
+def fetch_credentials():
+    email = request.form["email"]
+    passkey = request.form["passkey"]
+    if (email,passkey) in user_credentials:
+        session_exists = True
+        return render_template("products.html")
+    else:
+        return render_template("error.html")
+
+@app.route("/cart",methods=["POST"])
+def fetch_cart():
+    cart_items = request.form["cart_items"]
+    return render_template("cart.html",cart=cart_items)
